7a2f34999660fe92b59702676ddef90f0dde5a27
stanley chukwu <stanleychukwu17@yahoo.com>
2022-10-03 17:47:35 +0100

    started the design of the front page brought in the svg assets to be used for this project

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,7 +5,19 @@ const App = () => {
 
     return (
         <div className="AppMain">
-
+            <div className="leftCvr">
+                <div className="logoO">STANLEY</div>
+                <div className="">
+                    <div className="">X</div>
+                </div>
+            </div>
+            <div className="rightCvr">
+                <div className="">Search bar</div>
+                <div className="">Notes</div>
+                <div className="">
+                    <div className="">blocks</div>
+                </div>
+            </div>
         </div>
     )
 }
