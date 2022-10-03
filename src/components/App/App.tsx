6a030a16790d774f1853ad75b7849956b4ce1f43
stanley chukwu <stanleychukwu17@yahoo.com>
2022-10-03 19:30:54 +0100

    brought in the pencil that will be used for editing of notes done with the design, we go into the animation phase

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,8 @@
 // imports of stylesheet and other assets
 import './app.scss';
 import cancel from '../../assets/svg/cancel2.svg'
+import search from '../../assets/svg/zoom.svg'
+import pencil from '../../assets/svg/pencil.svg'
 
 const App = () => {
 
@@ -10,17 +12,30 @@ const App = () => {
         <div className="AppMain">
             <div className="leftCvr">
                 <div className="logoO">STANLEY</div>
-                <div className="">
+                <div className="lgAdd_Ovr">
                     <div className="">
                         <img src={cancel} alt="" />
                     </div>
                 </div>
             </div>
             <div className="rightCvr">
-                <div className="">Search bar</div>
-                <div className="">Notes</div>
+                <div className="search_cvr">
+                    <div className=""><img src={search} alt="" /></div>
+                    <div className="">Search</div>
+                </div>
+                <div className="titleO">Notes</div>
                 <div className="">
-                    <div className="">blocks</div>
+                    <div className="eNotes">
+                        <div className="eNotes__top">
+                            Magna excepteur deserunt est velit est eu exercitation do tempor. Ipsum laboris deserunt magna voluptate adipisicing ipsum. Exercitation elit nostrud do ex ipsum mollit est. Reprehenderit est eiusmod non quis qui do amet ex irure.
+                        </div>
+                        <div className="eNotes__bottom">
+                            <div className="date">May 22, 2022</div>
+                            <div className="pencil">
+                                <div className=""><img src={pencil} alt="" /></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
